cd34a516b924081de15e789e482a7ffd52278faf
Darshan Kumar <kumardarshan425@gmail.com>
2026-02-16 17:06:47 -0600

    Created basic thread class

--- Task_2.java
+++ Task_2.java
@@ -1,17 +1,51 @@
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
 public class Task_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        int R = getValidNum(scanner, 1, 10000, "Provide R where R is the number of reading-agents: ");
-        int W = getValidNum(scanner, 1, 10000, "Provide W where W is the number of writing-agents: ");
-        int N = getValidNum(scanner, 1, R, "Provide N where N is the maximum number of reading-agents that may access the shared resource at once: ");
+        int R = getValidNum(scanner, 1, 10000, "How many reading threads should be created? (integer from 1-10000): ");
+        int W = getValidNum(scanner, 1, 10000, "How many writer threads should be created? (integer from 1-10000): ");
+        int N = getValidNum(scanner, 1, R, "How many readers should be allowed to read at once? (integer from 1-R): ");
 
+        // Shared resource synchronization primitives
+        Semaphore writerMutex = new Semaphore(1);
+        Semaphore readerSemaphore = new Semaphore(N);
+
+        // Starts reader threads
+        for (int i = 0; i < R; i++) {
+            new myThreads(writerMutex, readerSemaphore, false, i).start();
+        }
+        // Starts writer threads
+        for (int i = 0; i < W; i++) {
+            new myThreads(writerMutex, readerSemaphore, true, i).start();
+        }
     }
 
 
-    public static int getValidNum(Scanner scanner, int min, int max, String prompt) {
+    private static class myThreads extends Thread {
+        Semaphore writerMutex;
+        Semaphore readerSemaphore;
+        boolean isWriter;
+        int ID;     // Thread ID for logging purposes
+
+        // Constructor
+        public myThreads(Semaphore writerMutex, Semaphore readerSemaphore, boolean isWriter, int ID) {
+            this.writerMutex = writerMutex;
+            this.readerSemaphore = readerSemaphore;
+            this.isWriter = isWriter;
+            this.ID = ID;
+        }
+       
+        @Override
+        public void run() {
+
+        }
+    }
+
+
+    private static int getValidNum(Scanner scanner, int min, int max, String prompt) {
         System.out.print(prompt);
         while (true) {
             try {
