b81314421d14b43dd89d4494f36267aa850d5675
Darshan Kumar <kumardarshan425@gmail.com>
2026-02-16 23:03:37 -0600

    mostly done, still facing writers deadlock

--- Task_2.java
+++ Task_2.java
@@ -10,62 +10,92 @@ public class Task_2 {
         int N = getValidNum(scanner, 1, R, "How many readers should be allowed to read at once? (integer from 1-R): ");
 
         // Shared resource synchronization primitives
-        Semaphore writerMutex = new Semaphore(1);
-        Semaphore readerSemaphore = new Semaphore(N);
+        Semaphore writersMutex = new Semaphore(1);      // Init readers count
+        Semaphore readersSemaphore = new Semaphore(N);          // Init readers count
+        Semaphore readersTurnMutex = new Semaphore(N);          // init readers turn
 
         // Starts reader threads
         for (int i = 0; i < R; i++) {
-            new myThreads(writerMutex, readerSemaphore, false, i).start();
+            new myThreads(writersMutex, readersSemaphore, readersTurnMutex, true, i, N).start();
         }
         // Starts writer threads
         for (int i = 0; i < W; i++) {
-            new myThreads(writerMutex, readerSemaphore, true, i).start();
+            new myThreads(writersMutex, readersSemaphore, readersTurnMutex, false, i, N).start();
         }
     }
 
 
     private static class myThreads extends Thread {
-        Semaphore writerMutex;
-        Semaphore readerSemaphore;
-        boolean isReader;           // Reader vs Writer ID
-        int ID;                     // Reader/Writer Thread ID
-        static Semaphore readersTurn = new Semaphore(1);
-        static Semaphore writersTurn = new Semaphore(1);
+        Semaphore writersMutex, readersSemaphore, readersTurnMutex;
+        boolean isReader;
+        int ID;
+        static int activeReadersCount = 0, maxReaders;
+        // Init readers turn
+        static Semaphore activeReadersCountMutex = new Semaphore(1);
+        static Semaphore writersTurnMutex = new Semaphore(0);
+
 
         // Constructor
-        public myThreads(Semaphore writerMutex, Semaphore readerSemaphore, boolean isWriter, int ID) {
-            this.writerMutex = writerMutex;
-            this.readerSemaphore = readerSemaphore;
-            this.isReader = isWriter;
+        public myThreads(Semaphore writersMutex, Semaphore readersSemaphore, Semaphore readersTurnMutex, boolean isReader, int ID, int maxReaders) {
+            this.writersMutex = writersMutex;
+            this.readersSemaphore = readersSemaphore;
+            this.readersTurnMutex = readersTurnMutex;
+            this.isReader = isReader;
             this.ID = ID;
+            Task_2.myThreads.maxReaders = maxReaders;   // Access the class static var instead of instances var
         }
        
+
         @Override
         public void run() {
             if (isReader) {
                 try {
-                    readersTurn.acquire();
-                    readerSemaphore.acquire();
-                    System.out.println("Reader " + ID + " is reading.");
-                    Thread.sleep(1000); // Simulate reading time
-                    writersTurn.release();
-                    readerSemaphore.release();
+                    // Allow N readers during this readers turn
+                    /*
+                     *   Next reader after max readers shall reset the active 
+                     *   reader count and wait for readersTurnMutex to be 
+                     *   released by the writer.
+                     */ 
+                    readersTurnMutex.acquire();             // Wait until readers turn and/or current readers batch is done
+                    activeReadersCountMutex.acquire();
+                    activeReadersCount++;
+                    activeReadersCountMutex.release();
+
+
+                    // Aquire permit to read
+                    readersSemaphore.acquire();
+                    // Simulate reading time
+                    System.out.println("-R" + (ID + 1) + " began reading.");
+                    Thread.sleep(1000);
+                    System.out.println("--R" + (ID + 1) + " finished reading.");
+
+                    // Done reading, release readers permit
+                    readersSemaphore.release();
+                    // Decriment active readers count
+                    activeReadersCountMutex.acquire();
+                    activeReadersCount--;
+                    // Are you the last reader? If so switch it to writers turn.
+                    if (activeReadersCount == 0) {
+                        writersTurnMutex.release();
+                    }
+                    activeReadersCountMutex.release();
                 } 
                 catch (Exception e) {
-
+                    e.printStackTrace();  
                 }
             }
             else {
                 try {
-                    writersTurn.acquire();
-                    writerMutex.acquire();
-                    System.out.println("Writer " + ID + " is writing.");
+                    writersTurnMutex.acquire();
+                    writersMutex.acquire();
+                    System.out.println("---W" + (ID + 1) + " began writing.");
                     Thread.sleep(1000); // Simulate writing time
-                    readersTurn.release();
-                    writerMutex.release();
+                    System.out.println("----W" + (ID + 1) + " finished writing.");
+                    readersTurnMutex.release(maxReaders);
+                    writersMutex.release();
                 } 
                 catch (Exception e) {
-
+                    e.printStackTrace();  
                 }
             }
         }
