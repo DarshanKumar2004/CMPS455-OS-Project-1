a0b41330179032ad87bb2d012620843f3c9abde3
ABOGGSLAPTOP\ihast <agyboggs@gmail.com>
2026-02-22 17:11:21 -0600

    Task 1 completed

---
+++ project1_C00513558/src/Barrier.java
@@ -0,0 +1,29 @@
+
+import java.util.concurrent.Semaphore;
+
+public class Barrier {
+    private int count = 0;
+    private int total;
+    private String status;
+
+    private Semaphore mutex = new Semaphore(1);
+    private Semaphore unlock = new Semaphore(0);
+    public Barrier (int total, String status) {
+        this.total = total;
+        this.status = status;
+    }
+
+    public void await() throws InterruptedException {
+        mutex.acquire();
+        count++;
+
+        // Only release threads when all have been arrived to same point
+        if (count == total) {
+            unlock.release(total);
+            System.out.println(status);
+        }
+
+        mutex.release();
+        unlock.acquire();
+    }
+}
---
+++ project1_C00513558/src/Lock.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.Semaphore;
+
+public class Lock {
+    private final Semaphore mutex = new Semaphore(1);
+
+    public void lock() throws InterruptedException {
+        mutex.acquire();
+    }
+
+    public void unlock() {
+        mutex.release();
+    }
+}
